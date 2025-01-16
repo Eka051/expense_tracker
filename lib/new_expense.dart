b6e12d4e8d845fc613787039d71ba8ea6ec02a14
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-16 22:31:57 +0700

    feat: enhance NewExpense layout with date selection and improved input structure

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -33,15 +34,33 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: InputDecoration(labelText: 'Title'),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
-            ],
-            decoration: InputDecoration(
-              labelText: 'Amount',
-              prefix: Text('Rp. '),
+          Expanded(
+            child: Row(
+              children: [
+                TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
+                  ],
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
+                    prefix: Text('Rp. '),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Row(
+              children: [
+                Text('Selected Date: '),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.calendar_month),
+                ),
+              ],
             ),
           ),
           const SizedBox(
@@ -52,7 +71,7 @@ class _NewExpenseState extends State<NewExpense> {
               // DropdownButton(items: Category(), onChanged: () {}),
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop();
+                  Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
               ),
