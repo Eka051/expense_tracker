bcb5e806a973449a612da3f2a20055d094aa5080
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-18 19:50:11 +0700

    feat: implement expense removal functionality in ExpensesList

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -51,6 +51,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +73,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           Text('Chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
